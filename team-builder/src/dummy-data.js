7ab59a77396497b93b4c4b990d8c2b8887fc0bba
Mandi Haase <haase1020@gmail.com>
2020-01-30 07:05:24 -0500

    add dummy data

---
+++ team-builder/src/dummy-data.js
@@ -0,0 +1,32 @@
+const dummyData = [
+    {
+        name: "Mandi Haase",
+        email:"haase1020@gmail.com",
+        role: "student"
+    },
+    {
+        name: "Zachary Smith",
+        email:"zachsmith@gmail.com",
+        role: "leader"
+    },
+    {
+        name: "Juana Villalpando",
+        email: "juanavilla@gmail.com",
+        role: "student"
+    },
+    {
+        name: "Richard Lynch",
+        email: "richlynch@gmail.com",
+        role: "student"
+    },
+    {
+        name: "Cedric Winbush",
+        email: "cedricwinbush@gmail.com",
+        role: "student"
+    },
+    {
+        name: "Zachary Stack",
+        email: "zachstack@gmail.com",
+        role: "student"
+    },
+]
